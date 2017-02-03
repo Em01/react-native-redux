f0a0344eecb59a51cd850fc919ed2962304deb9f
Em01 <emma_williams1@hotmail.co.uk>
2017-02-03 09:18:14 +0000

    rename and fix setup

--- ios/redux-nativeTests/redux-nativeTests.m
+++ ios/redux-nativeTests/redux-nativeTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface reduxTests : XCTestCase
+@interface redux-nativeTests : XCTestCase
 
 @end
 
-@implementation reduxTests
+@implementation redux-nativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
